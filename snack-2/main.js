f1c807f243b982ecfc0a55bc2c815f966b3d07e3
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-13 11:30:43 +0100

    edit snack 2

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,11 +1,11 @@
 let primaParola = prompt ("inserisci una parola")
 let secondaParola = prompt ("inserisci una parola")
 
-if (primaParola.length < secondaParola.length){
+if (primaParola.length > secondaParola.length){
     console.log(secondaParola, primaParola)
     
 }
-else if (secondaParola.length < primaParola.length){
+else if (secondaParola.length > primaParola.length){
     console.log(primaParola, secondaParola)
     
 }
