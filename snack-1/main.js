57b249ae077d334d480e700f97c21d984dbefef5
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-13 10:39:34 +0100

    snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,11 @@
+let numeroMaggiore =  prompt ("inserisci il numero")
+let numeroMinore = prompt ("inserisci un numero")
+if (numeroMaggiore > numeroMinore){
+    console.log(numeroMaggiore);
+}
+
+else if (numeroMinore > numeroMaggiore){
+console.log(numeroMinore);
+}
+
+
